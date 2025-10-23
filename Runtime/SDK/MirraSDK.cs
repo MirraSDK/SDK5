68707a9eb43160a6bc103ec9c55d8c8c26690af7
Roman Lee ツ <git@romanlee17.com>
2025-10-23 17:48:44 +0300

    vk: add free cloud saves support

--- Runtime/SDK/MirraSDK.cs
+++ Runtime/SDK/MirraSDK.cs
@@ -6,7 +6,7 @@ namespace MirraGames.SDK {
     [Root, Preserve]
     public partial class MirraSDK {
 
-        public const string Version = "5.0.7";
+        public const string Version = "5.0.8";
 
         private MirraSDK(MainFactory factory) {
             Logger.CreateText(this, $"Starting up. Version {Version}");

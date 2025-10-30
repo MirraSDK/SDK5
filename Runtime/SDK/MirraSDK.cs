a1a6a98d1409587a0cce02eb3a27e190ef95b77e
Roman Lee ツ <git@romanlee17.com>
2025-10-30 17:43:57 +0300

    version 5.0.13

--- Runtime/SDK/MirraSDK.cs
+++ Runtime/SDK/MirraSDK.cs
@@ -6,7 +6,7 @@ namespace MirraGames.SDK {
     [Root, Preserve]
     public partial class MirraSDK {
 
-        public const string Version = "5.0.12";
+        public const string Version = "5.0.13";
 
         private MirraSDK(MainFactory factory) {
             Logger.CreateText(this, $"Starting up. Version {Version}");

caf85d5ea777b8e78ef8fc9cd051ed3bf7d6eee1
Roman Likhadievski <git@romanlee17.com>
2026-01-28 22:33:05 +0300

    rebuild mirrasdk.js

--- Runtime/SDK/MirraSDK.cs
+++ Runtime/SDK/MirraSDK.cs
@@ -6,7 +6,7 @@ namespace MirraGames.SDK {
     [Root, Preserve]
     public partial class MirraSDK {
 
-        public const string Version = "5.1.7";
+        public const string Version = "5.1.8";
 
         private MirraSDK(MainFactory factory) {
             Logger.CreateText(this, $"Starting up. Version {Version}");

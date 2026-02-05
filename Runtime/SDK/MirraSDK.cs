6ece231272eb17558ad6c0b5678e81606ff35d58
Roman Likhadievski <git@romanlee17.com>
2026-02-05 12:53:22 +0300

    fix: web ads pause issue on android/chrome

--- Runtime/SDK/MirraSDK.cs
+++ Runtime/SDK/MirraSDK.cs
@@ -6,7 +6,7 @@ namespace MirraGames.SDK {
     [Root, Preserve]
     public partial class MirraSDK {
 
-        public const string Version = "5.1.9";
+        public const string Version = "5.1.11";
 
         private MirraSDK(MainFactory factory) {
             Logger.CreateText(this, $"Starting up. Version {Version}");

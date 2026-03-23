b1eca428e7b475c5943d3a8e7e460a6f2445a855
Roman Likhadievski <git@romanlee17.com>
2026-03-23 17:43:03 +0300

    sdk: version 5.1.19

--- Runtime/SDK/MirraSDK.cs
+++ Runtime/SDK/MirraSDK.cs
@@ -6,7 +6,7 @@ namespace MirraGames.SDK {
     [Root, Preserve]
     public partial class MirraSDK {
 
-        public const string Version = "5.1.18";
+        public const string Version = "5.1.19";
 
         private MirraSDK(MainFactory factory) {
             Logger.CreateText(this, $"Starting up. Version {Version}");
